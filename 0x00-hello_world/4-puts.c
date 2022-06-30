910d5f5dba79821aa8e06d5f04bb2f62cba40930
Kenza1525 <kerianzenzabampema@gmail.com>
2022-06-30 14:24:25 -0700

    Modified 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * return: 0 after printing function
+ */
+
+int main(void)
+{ 
+	char variable[] = "\" Programming is like building a multilingual puzzle";
+	puts(variable);
+	return 0;
+}
