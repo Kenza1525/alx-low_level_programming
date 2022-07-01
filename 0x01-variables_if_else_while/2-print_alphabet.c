a9460819d8153a4ecd3b10965b8af3f977e89ed8
Kenza1525 <kerianzenzabampema@gmail.com>
2022-07-01 13:20:27 -0700

    Added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -> it will return the alphabet in lower case
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	char alpha;
+
+	srand(time(0));
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha = alpha + 1;
+	}
+	putchar('\n');
+	return (0);
+}
