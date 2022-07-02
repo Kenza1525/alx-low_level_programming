d19faed2490a914126c3c3e5175467e0065001a4
Kenza1525 <kerianzenzabampema@gmail.com>
2022-07-01 18:33:51 -0700

    Modified 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,24 +6,31 @@
  */
 int main(void)
 {
-	int digit1;
-	int digit2;
+	int digit1 = 0;
+	int digit2 = 0;
 
-	for (digit1 = 0; digit1 < 98; digit1++)
+	while (digit1 <= 98)
 	{
-		for (digit2 = digit1 + 1; digit2 <= 99; digit2++)
+		while (digit2 <= 99)
 		{
-			putchar((digit1 / 10) + '0');
-			putchar((digit1 % 10) + '0');
-			putchar(' ');
-			putchar((digit2 / 10) + '0');
-			putchar((digit2 % 10) + '0');
-			if ((digit1 == 98) && (digit2 == 99))
-				continue;
+			if (digit1 < digit2)
+			{
+				putchar((digit1 / 10) + '0');
+				putchar((digit1 % 10) + '0');
+				putchar(' ');
+				putchar((digit2 / 10) + '0');
+				putchar((digit2 % 10) + '0');
 
-			putchar(',');
-			putchar(' ');
+				if (digit1 != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			digit2++;
 		}
+		digit1++;
+
 	}
 	putchar('\n');
 	return (0);
