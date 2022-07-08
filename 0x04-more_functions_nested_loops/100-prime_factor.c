0a5791d3a91168cbd1fc18f1d102e9bdb3ed76e1
Kenza1525 <kerianzenzabampema@gmail.com>
2022-07-07 17:10:42 -0700

    Added 100-prime_factors

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,75 +1,20 @@
 #include <stdio.h>
-#include "main.h"
+
 /**
- * main - prints the biggest prime factor of a number.
- *
- * Return: Always 0.
- */
+* main - Entry point.
+* Return: Always 0.
+*/
+
 int main(void)
+
 {
-	long int digit;
-
-	digit = 612852475143;
-
-	if (isPrime(digit) == 1)
-	{
-		printf("%ld\n", digit);
-	}
-	else
-	{
-		printf("%ld\n", biggestFactor(digit));
-	}
-	return (0);
+long number, i;
+number = 612852475143;
+for (i = 2; i < number; i++)
+{
+while (number % i == 0)
+number = number / i;
 }
-
-int isPrime(long int n)
-{
-	int num;
-
-	if (n <= 1)
-	{
-		return (0);
-	}
-	else if (n == 2)
-	{
-		return (1);
-	}
-	else
-	{
-		for (num = 2; num < n; a++)
-		{
-			if (n % i == 0)
-			{
-				return (0);
-			}
-		}
-	return (1);
-	}
-}
-
-long int biggestFactor(long int a)
-{
-	long int f, factor;
-
-	factor = a;
-	for (f = 2; f <= factor; f++)
-	{
-		if (isPrime(factor) == 1)
-		{
-			break;
-		}
-		else
-		{
-			if ((factor % f == 0) && (isPrime(f) == 1))
-			{
-				factor = factor / f;
-				continue;
-			}
-			else
-			{
-				factor = factor;
-			}
-		}
-	}
-	return (factor);
+printf("%lu\n", number);
+return (0);
 }
