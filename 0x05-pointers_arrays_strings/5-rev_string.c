3b7ed1e059520f95639c8527446a89a9df968cdc
Kenza1525 <kerianzenzabampema@gmail.com>
2022-07-11 09:41:20 -0700

    Added 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: pointer that stores a character
+ *
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int index, n;
+	char store[20];
+
+	index = 0;
+	n = 0;
+	while (s[index] != '\0')
+	{
+		store[index] = s[index];
+		index++;
+	}
+	index--;
+	while (index >= 0)
+	{
+		s[index] = store[n];
+		index--;
+		n++;
+	}
+
+}
