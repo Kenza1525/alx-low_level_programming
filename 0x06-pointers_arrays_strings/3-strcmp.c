3564bc7742cdffc3fec5248fd84a7cbed0e0e8d3
Kenza1525 <kerianzenzabampema@gmail.com>
2022-07-13 10:14:33 -0700

    Added 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer that stores a string
+ * @s2: pointer that stores a string
+ *
+ * Return: an integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int diff;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			return (diff);
+		}
+		else
+		{
+			return (0);
+		}
+		i++;
+	}
+}
