49e8ecd6018b5b0932dbe1571b8c46f38af35c1f
Kenza1525 <kerianzenzabampema@gmail.com>
2022-07-18 09:11:20 -0700

    Added 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,20 +8,16 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j;
+	int i, j;
 
-	for (i = 0; s[i]; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j]; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				break;
+				return (&s[i]);
 			}
-		}
-		if (accept[j] != '\0')
-		{
-			return (s);
 		}
 	}
 	return (0);
