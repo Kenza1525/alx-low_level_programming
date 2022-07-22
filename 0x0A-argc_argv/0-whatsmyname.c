f0f671b66cdbc797754b7aecc8a0fbc090f6d7ac
Kenza1525 <kerianzenzabampema@gmail.com>
2022-07-22 07:07:20 -0700

    Added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the executable
+ * @argc: size of argv
+ * @argv: array of strings
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", *argv[0]);
+	return (0);
+}
