2c2d2e168d10bdcf279c605374529d79d1dd5ef6
Kenza1525 <kerianzenzabampema@gmail.com>
2022-07-22 13:03:22 -0700

    Added 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,27 +1,38 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 /**
- * main - print the addition of two positive numbers.
+ * main - print the sum
  * @argc: argv size
- * @argv: array
- *
- * Return: 1 for less of 2 arguments or nondigit numbers, 0 success
+ * @argv: array 
+ * Return: 1 or 0
  */
-int main(int argc, char **argv)
-{
-	int sum, i, j;
 
-	sum = 0;
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i][j]))
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		sum += atoi(argv[i]);
+			sum += atoi(argv[i]);
 	}
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		printf("%i\n", sum);
+	}
+	return (0);
+}
