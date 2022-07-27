24870d6bbb3a45722ccac367f4c320e6755e2c6d
Kenza1525 <kerianzenzabampema@gmail.com>
2022-07-26 21:48:38 -0700

    Modified 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,9 +21,9 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[i] != '\0')
+	while (s1[size1] != '\0')
 		size1++;
-	while (s2[i] != '\0')
+	while (s2[size2] != '\0')
 		size2++;
 	size2++;
 
