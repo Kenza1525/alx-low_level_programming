191e4e68d65db2c152205b3adf5358b98801ee56
Kenza1525 <kerianzenzabampema@gmail.com>
2022-08-18 06:44:35 -0700

    Added 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * binary_rep - recursion function to print bit 0 (acts as a mask)
+ * @n: type const unsigned long int
+ */
+
+void binary_rep(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	binary_rep(n >> 1);
+	_putchar((n & 1) + '0');
+}
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: type const unsigned long int
+ * Return: binary number
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else
+		binary_rep(n);
+}
