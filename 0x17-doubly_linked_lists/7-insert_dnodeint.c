97fd08b278ffcb0a59a300bad66f150f6b47e7eb
kenza1525 <kerianenzabampema@gmail.com>
2022-09-29 16:07:09 -0700

    Added from 5

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - insert a node at a given index
+ * @h: head node
+ * @idx: index to insert the node
+ * @n: the node value
+ * Return: newNode
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp = *h;
+	dlistint_t *newNode = malloc(sizeof(dlistint_t));
+
+	if (newNode == NULL)
+		return (NULL);
+	if (idx == 0)
+		return	(add_dnodeint(h, n));
+
+	while (idx > 1)
+	{
+		if (tmp == NULL)
+			return (NULL);
+		tmp = tmp->next;
+		idx--;
+	}
+	if (tmp->next == NULL)
+		return	(add_dnodeint_end(h, n));
+	newNode->n = n;
+	newNode->prev = tmp;
+	newNode->next = tmp->next;
+	tmp->next->prev = newNode;
+	tmp->next = newNode;
+	return (newNode);
+}
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - delete a node at a given index
+ * @head: head node
+ * @index: the index that the node is goint to be deleted
+ * Return: 1 on succeeded, 0 fail
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp = *head;
+
+	if (*head == NULL)
+		return (-1);
+	while (index > 0)
+	{
+		if (tmp == NULL)
+			return (-1);
+		tmp = tmp->next;
+		index--;
+	}
+
+	if (tmp == *head)
+	{
+		*head = tmp->next;
+		(*head)->prev = NULL;
+	}
+	else
+	{
+		tmp->prev->next = tmp->next;
+		if (tmp->next)
+			tmp->next->prev = tmp->prev;
+	}
+	free(tmp);
+	return (1);
+}
