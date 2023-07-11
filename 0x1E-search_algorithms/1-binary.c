b8b42acdc91568619eb58c57eb50acecf3d3b716
Keriane Nzabampema <kerianenzabampema@gmail.com>
2023-07-11 13:06:45 +0200

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -16,9 +16,8 @@ int binary_search(int *array, size_t size, int value)
 	size_t i;
 
 	if (array == NULL)
-	{
 		return (-1);
-	}
+
 	while (first_element <= last_element)
 	{
 		mid_element = (first_element + last_element) / 2;
