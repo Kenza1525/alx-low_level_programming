26b9a2409582289f3107f1ac2abafbea6d81358e
Kenza1525 <kerianenzabampema@gmail.com>
2023-07-11 03:59:24 -0700

    Reverted to first code

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -17,7 +17,7 @@ int binary_search(int *array, size_t size, int value)
 
 	while (first_element <= last_element)
 	{
-		mid_element = first_element + (last_element - first_element) / 2;
+		mid_element = (first_element + last_element) / 2;
 		printf("Searching in array: ");
 
 		for (i = first_element; i < last_element + 1; i++)
